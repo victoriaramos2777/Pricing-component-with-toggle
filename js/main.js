f4a06f941b59bc5615f9f6af87f7b33242123800
Victoria Ramos <777vikiramos22@gmail.com>
2023-01-16 21:12:56 -0600

    estructura del html terminada y estilos de css iniciada

--- js/main.js
+++ js/main.js
@@ -1,11 +1,2 @@
-import '../assets/styles/style.scss'
-import javascriptLogo from '../assets/logos/JavaScript.svg'
-import { setupCounter } from './modules/counter.js'
+import '../assets/styles/style.css'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <p>Esta es un aplantilla de vanilla js editada con vite.</p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
