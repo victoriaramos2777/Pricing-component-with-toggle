eac136082ebba18bd1fe3f4a233e7cef5066c9a2
Victoria Ramos <777vikiramos22@gmail.com>
2023-01-16 22:47:22 -0600

    agregue evento click y la funcion de seleccionar en js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
-import '../assets/styles/style.css'
+import "../assets/styles/style.css";
+//constante para llamar el selector
+const selector = document.getElementById("selector-barra");
 
+// evento escucha para el selector-barra
+selector.addEventListener("click", myFuncion);
+// funcion para selecionar
+function myFuncion() {
+  let mensual = document.getElementsByTagName("h3");
+  let anual = document.getElementsByTagName("h4");
+
+  for (let i = 0; i < mensual.length; i++) {
+    for (let x = 0; x < anual.length; x++) {
+      if (mensual[i].style.display === "none") {
+        mensual[i].style.display = "block";
+        anual[i].style.display = "none";
+        selector.style.justifyContent = "flex-end";
+      } else {
+        mensual[i].style.display = "none";
+        anual[i].style.display = "block";
+        selector.style.justifyContent = "flex-start";
+      }
+    }
+  }
+}
